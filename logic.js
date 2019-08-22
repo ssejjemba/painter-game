831fbb1686b3b210c3a2bcecfb794b9501161de8
Daniel <ssejjembadan@gmail.com>
2019-08-22 17:58:26 +0300

    Add origin to ballon sprite

--- logic.js
+++ logic.js
@@ -6,7 +6,13 @@ var Game = {
     backgroundSprite : undefined,
     balloonSprite : undefined,
     balloonPosition : { x : 0, y : 0 },
+    balloonOrigin : { x : 0, y : 0 },
     backgroundMusic: undefined,
+    cannonBarrelSprite : undefined,
+    mousePosition : { x : 0, y : 0 },
+    cannonPosition : { x : 72, y : 405 },
+    cannonOrigin : { x : 34, y : 34 },
+    cannonRotation : 0
 };
 
 Game.start = function () {
@@ -42,15 +48,18 @@ Game.update = function () {
 };
 
 Game.draw = function () {
-    Game.drawImage(Game.backgroundSprite, { x : 0, y : 0 });
-    Game.drawImage(Game.balloonSprite, Game.balloonPosition);
+    Game.balloonOrigin = { x : Game.balloonSprite.width / 2,
+        y : Game.balloonSprite.height };
+    Game.drawImage(Game.backgroundSprite, { x : 0, y : 0 }, 0, { x : 0, y : 0 });
+    Game.drawImage(Game.balloonSprite, Game.balloonPosition, 0, Game.balloonOrigin);
 };
 
-Game.drawImage = function (sprite, position) {
+Game.drawImage = function (sprite, position, rotation, origin) {
     Game.canvasContext.save();
     Game.canvasContext.translate(position.x, position.y);
+    Game.canvasContext.rotate(rotation);
     Game.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height,
-        0, 0, sprite.width, sprite.height);
+        -origin.x, -origin.y, sprite.width, sprite.height);
     Game.canvasContext.restore();
 };
 
